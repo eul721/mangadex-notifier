1d7a46c2f233654bd69a30a8817188cfe68d35c0
Jack Chao <euleriepsilon@gmail.com>
2019-03-13 00:13:57 -0700

    Allowing retries on failed rss-poll

--- src/mangadex-notifier.js
+++ src/mangadex-notifier.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Parser = require('rss-parser')
+const retry = require('async-retry')
 const fetch = require('node-fetch')
 const parser = new Parser();
 
@@ -9,7 +10,10 @@ const rssFeedUUID = process.env.rssFeedUUID
 const DiscordWebhookUrl = `https://discordapp.com/api/webhooks/${discordWebhookToken}`
 
 module.exports.index = async (event, context, cb) => {
-    const items = (await parser.parseURL(`https://mangadex.org/rss/follows/${rssFeedUUID}`)).items;
+    const rssResponse = await retry(async (fn,i)=>{
+        return await parser.parseURL(`https://mangadex.org/rss/follows/${rssFeedUUID}`);
+    },{retries:5})
+    const items = rssResponse.items;
     const nowDate = new Date();
 
     let i = 0
